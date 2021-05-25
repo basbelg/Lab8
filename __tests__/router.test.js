d9243be6dc369f3c3ea72cc97fd52d0ed16f272d
basbelg <basbeez@live.com>
2021-05-24 23:30:58 -0700

    updates

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,11 @@
  * @jest-environment jsdom
  */
 
+import { TestScheduler } from "@jest/core";
+import { pushToHistory } from '../scripts/router.js';
+
+/*describe('Push to History', () => {
+    test('settings', () => {
+        expect(pushToHistory('settings', 0)).toEqual(history.pushState({ page: 'settings' }, '', './#settings'));
+    });
+});*/
